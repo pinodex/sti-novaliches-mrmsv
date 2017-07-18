7042f302262644818baa695b5337014e81b43a7e
Raphael Marco <raphaelmarco@outlook.com>
2017-07-18 22:28:14 +0800

    Add files

---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+let options = {
+    processCssUrls: false
+}
+
+mix
+  .sass('resources/assets/sass/main.scss', 'public/assets/css')
+  .sass('resources/assets/sass/bulma.scss', 'public/assets/css')
+
+  .js('resources/assets/js/app.js', 'public/assets/js')
+
+  .copy('resources/assets/img', 'public/assets/img')
+  .copy('resources/assets/icon', 'public/assets/icon')
+  .copy('node_modules/material-design-icons/iconfont', 'public/assets/fonts')
+
+  .extract(['vue'])
+
+  .disableNotifications()
+  .setPublicPath('public')
+  .options(options)
